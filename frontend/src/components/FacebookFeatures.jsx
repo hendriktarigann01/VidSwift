626771f10c2365845974d258c1fc0baa50d13dcd
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-10-21 23:58:11 +0700

    active button instagram features

--- frontend/src/components/FacebookFeatures.jsx
+++ frontend/src/components/FacebookFeatures.jsx
@@ -3,7 +3,7 @@ import FacebookFollowers from "./feature/FacebookFollowers";
 import FacebookLike from "./feature/FacebookLike";
 
 const FacebookFeatures = () => {
-  const [selectedFeature, setSelectedFeature] = useState(null); // State untuk menyimpan fitur yang dipilih
+  const [selectedFeature, setSelectedFeature] = useState(null); 
 
   const handleButtonClick = (feature) => {
     setSelectedFeature(feature);
@@ -14,13 +14,17 @@ const FacebookFeatures = () => {
       <h2 className="text-lg sm:text-xl font-bold mb-4">Facebook</h2>
       <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4 mb-4">
         <button
-          className="w-32 px-4 py-2 bg-white border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-lg"
+          className={`w-32 px-4 py-2 bg-white border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-lg ${
+            selectedFeature === "like" ? "bg-blue-400 text-white" : ""
+          }`}
           onClick={() => handleButtonClick("like")}
         >
           Add Like
         </button>
         <button
-          className="w-32 px-4 py-2 bg-white border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-lg"
+          className={`w-32 px-4 py-2 bg-white border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-lg ${
+            selectedFeature === "followers" ? "bg-blue-400 text-white" : ""
+          }`}
           onClick={() => handleButtonClick("followers")}
         >
           Add Followers
--- frontend/src/components/InstagramFeatures.jsx
+++ frontend/src/components/InstagramFeatures.jsx
@@ -14,13 +14,21 @@ const InstagramFeatures = () => {
       <h2 className="text-lg sm:text-xl font-bold mb-4">Instagram</h2>
       <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4 mb-4">
         <button
-          className="w-32 px-4 py-2 bg-white border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white rounded-lg"
+          className={`w-32 px-4 py-2 bg-white border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white rounded-lg ${
+            selectedFeature === "like"
+              ? "bg-pink-300 text-white font-medium"
+              : ""
+          }`}
           onClick={() => handleButtonClick("like")}
         >
           Add Like
         </button>
         <button
-          className="w-32 px-4 py-2 bg-white border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white rounded-lg"
+          className={`w-32 px-4 py-2 bg-white border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white rounded-lg ${
+            selectedFeature === "followers"
+              ? "bg-pink-300 text-white font-medium"
+              : ""
+          }`}
           onClick={() => handleButtonClick("followers")}
         >
           Add Followers
