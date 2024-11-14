a30254f577568e608789f1c30561b57cfc315a34
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-11-14 22:32:19 +0700

    update navbar

--- frontend/src/navbar/Footer.jsx
+++ frontend/src/navbar/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-500 rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-blue-500 rounded-lg shadow m-2 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-white sm:text-center dark:text-gray-400">
           © 2024 <a className="cursor-auto">VidSwift</a>. All Rights Reserved.
--- frontend/src/navbar/Header.jsx
+++ frontend/src/navbar/Header.jsx
@@ -52,7 +52,7 @@ const Header = ({ setLoading }) => {
 
   return (
     <header>
-      <nav className="bg-blue-500 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="bg-blue-500 rounded-lg m-2 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="relative flex flex-wrap items-center justify-between mx-auto max-w-screen-xl">
           <a href="https://flowbite.com" className="flex items-center">
             <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
