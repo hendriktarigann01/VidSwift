3c5a4fbcfc55fed9af864ed82b6b28048214be66
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-17 23:35:56 +0700

    remove navbar /tutorial

--- frontend/src/navbar/Header.jsx
+++ frontend/src/navbar/Header.jsx
@@ -111,14 +111,14 @@ const Header = ({ setLoading }) => {
                   Download
                 </a>
               </li>
-              <li className="block">
+              {/* <li className="block">
                 <a
                   href="/tutorial"
                  className="block items-center py-2 pr-4 pl-3 text-white hover:text-blue-950 rounded lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 dark:hover:text-slate-200"
                 >
                   Tutorial
                 </a>
-              </li>
+              </li> */}
               <li className="block">
                 <Link
                   to={isLoggedIn ? "/generate-image" : "#"}
